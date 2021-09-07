9ec02ca24a61659044c8b3ed71e678b5668c86a2
zzzqqqzzz <1713210651@qq.com>
2021-09-07 16:00:46 +0800

    first commit

---
+++ jiadian/src/main/java/com/zzz/jiadian/JiadianApplication.java
@@ -0,0 +1,13 @@
+package com.zzz.jiadian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiadianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiadianApplication.class, args);
+    }
+
+}
